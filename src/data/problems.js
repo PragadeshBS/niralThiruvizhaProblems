2dc9075431b19025cac1bdf97b9ca091f55f4293
Pragadesh BS <pragadeshbs@protonmail.com>
2024-01-22 20:27:22 +0530

    initial commit

---
+++ src/data/problems.js
@@ -0,0 +1,292 @@
+const agri = [
+  [
+    "How might we implement semi-automation for the raw cutting process in cashew processing to reduce dependency, increase efficiency, and enhance overall productivity in the cashew industry?",
+    "66",
+  ],
+  [
+    "How might we develop suitable technology to estimate crop yield, leveraging technology to analyze agricultural data, optimize farming practices, and ensure accurate predictions for improved food production and security?",
+    "67",
+  ],
+  [
+    "How might we develop an innovative, thermally controlled storage system to address challenges in postharvest tomato storage, overcoming limitations in current preservation methods? The goal is to extend shelf life and minimize degradation, marking a significant stride in enhancing storage practices.",
+    "68",
+  ],
+  [
+    "How might we develop an effective Harvesting machine for crops that is lightweight, cost-effective, and convenient to handle and transport across various wet fields to address the challenges of the current model?",
+    "69",
+  ],
+  [
+    "How might we develop an affordable specialized vegetable harvester to improve automation and productivity in the agriculture sector, meeting the needs of the masses?",
+    "70",
+  ],
+  [
+    "How might we innovate in detection methods for mycotoxins, toxic secondary metabolites in food, to minimize contamination and ensure the safety of food processing units? The goal is to enhance current techniques, primarily chromatographic, for more effective results.",
+    "71",
+  ],
+  [
+    "How might we create a system that leverages satellite imagery and machine learning to detect vegetation height beneath transmission lines, anticipate growth patterns, and generate alerts for timely trimming when needed?",
+    "72",
+  ],
+  [
+    "How can we create an advanced agricultural bot to address challenges faced by small-scale farmers? This involves enhancing crop management, income, and food security through real-time soil testing, pest detection, and autonomous operations, with the aim of doubling agricultural output and income.",
+    "73",
+  ],
+  [
+    "How can we design safer alternatives and improved application techniques to address health hazards associated with pesticides, reducing human exposure and safeguarding both the environment and public health?",
+    "74",
+  ],
+  [
+    "How might we optimize water management in piped and micro irrigation? Integrating AI to predict crop water needs, automate valves, and boost yield.",
+    "75",
+  ],
+  [
+    "How might we develop image processing software using machine learning to identify medicinal plants, enhancing authenticity and ensuring integrity in the medicinal plant supply chain?",
+    "76",
+  ],
+  [
+    "How we design a cost-effective, energy-efficient small-scale cold storage unit for horticultural produce to extend perishable crop shelf life, specifically catering to the preservation needs of small-scale farmers.",
+    "77",
+  ],
+  [
+    "How might we innovate to address urban excess food waste, hunger-related deaths, and create a logistic network with innovative technology and cost-effective storage solutions? This aims to efficiently transfer surplus food from cities to areas in deficit.",
+    "78",
+  ],
+];
+const cleanAndGreen = [
+  [
+    "How might we develop a sustainable and eco-friendly extraction method for chitosan from shrimp shells to meet the increasing demand for chitosan across various industries?",
+    "79",
+  ],
+  [
+    "How can we create an automatic sensor model to address ammonia/H2S gas emissions from bird litter in the poultry sector, managing odor and fly issues? The goal is to assess gas emissions and trigger alerts when limits are exceeded, prompting necessary measures for effective management.",
+    "80",
+  ],
+  [
+    "How can we reduce the contribution of Cement Industries in global CO2 emissions and come up with alternate sustainable solutions.",
+    "81",
+  ],
+  [
+    "How we create a solution for efficient autonomous dust cleaning beneath roofs in industries, marriage halls, cinema theaters, and college auditoriums to maintain clean and healthy environments?",
+    "82",
+  ],
+  [
+    "How might we develop a low-cost automation system to address the labor-intensive manual extraction of dry coir pith? The industry requires an innovative suction system capable of efficiently collecting and transporting pith to manufacturing units, handling various materials simultaneously.",
+    "83",
+  ],
+  [
+    "How might we develop eco-friendly construction materials, specifically suitable for high-rise buildings, to mitigate the environmental impact of carbon dioxide emissions from cement production, exacerbated by urbanization and excessive concrete use?",
+    "84",
+  ],
+  [
+    "How can we design a solution to address the challenge of fugitive dust emissions from stone crusher units & crematorium causing air pollution?",
+    "85",
+  ],
+  [
+    "How might we design a cost-effective home composting solution for daily kitchen waste, ensuring quick and odor-free decomposition? The current lack of such an efficient system poses a challenge for homemakers seeking sustainable waste management.",
+    "86",
+  ],
+  [
+    "How might a comprehensive strategy be designed to mitigate the environmental impact of plastic degradation in the marine ecosystem? This multifaceted initiative seeks to create effective anti-pollutants, innovative methods for plastic disposal in oceans, tactics to boost algae growth, and the deployment of boats with robots for efficient plastic waste collection.",
+    "87",
+  ],
+];
+const educationFourPointO = [
+  [
+    "How might we create a skill/job recommender application using suitable technology, transforming career guidance by leveraging technology to match individuals with suitable jobs, fostering efficient employment and career development?",
+    "88",
+  ],
+  [
+    "How might we explore the adoption of game-based education to cultivate diverse skills, motivate students with points and leaderboards, and ensure inclusivity for students with disabilities? The goal is to enhance learning outcomes and confidence.",
+    "89",
+  ],
+];
+const healthMedTech = [
+  [
+    "How might we develop analytics for hospitals' health-care data, optimizing data utilization to improve patient care, streamline operations, and enhance overall efficiency in healthcare institutions?",
+    "90",
+  ],
+  [
+    "How might we design a cost-effective Myoelectric prosthetic arm using 3D printing, servo motors, Arduino, and Myoware muscle sensors, ensuring affordability without compromising functionality and quality?\r\n\r\n\r\n\r\n\r\n",
+    "91",
+  ],
+  [
+    "How can we design an image processing algorithm for portable X-ray devices to reduce noise, enhance contrast, and sharpen images without altering critical diagnostic details? This includes considering hardware limitations, ensuring computational efficiency, and validating with diverse datasets for accurate clinical application.",
+    "92",
+  ],
+  [
+    "How might we create a telemedicine robotic kiosk for rural India, utilizing AI, biometric ID, and the e-sanjeevani App for personalized access to expert doctors and timely medication delivery?",
+    "93",
+  ],
+  [
+    "How might we utilize AI chatbots and machine learning to address the challenges of incomplete alleviation of depression symptoms, attrition, and loss of follow-up in mental health treatment?",
+    "94",
+  ],
+];
+const heritage = [
+  [
+    "How might we develop a digital platform that creates a comprehensive repository, ensures authenticity, and fosters cross-generational dialogue for the appreciation and revitalization of diverse cultural elements to preserve endangered indigenous heritage and culture amidst modern challenges?",
+    "95",
+  ],
+];
+const renewableAndSustainableEnergy = [
+  [
+    "How might we develop a solution to enhance solar panel efficiency by addressing the challenges of dust and atmospheric moisture accumulation, which currently diminish their performance below intended levels?",
+    "96",
+  ],
+];
+const smartManufacturing = [
+  [
+    "How might we design cost effective personalized motorcycles and tricycles for individuals with disabilities, addressing their unique challenges and offering inclusive and accessible transportation solutions that cater to their specific needs and preferences?",
+    "97",
+  ],
+  [
+    "How might we safeguard sensitive data in IIoT systems? Addressing cybersecurity threats, securing data transmission, and implementing robust access controls are critical for ensuring the safe deployment of interconnected industrial solutions.",
+    "98",
+  ],
+  [
+    "How we innovate to enhance multimedia and animation, addressing challenges in realism, efficiency, cross-platform compatibility, compression, VR/AR, education, ethics, and inclusivity? The project aims is to promote innovative techniques and tools for immersive, inclusive, and ethical content creation across various industries.",
+    "99",
+  ],
+  [
+    "How might we create an innovative shopping assistance system using industrial automation specifically designed for disabled individuals? The focus is on integrating eye blink technology, elevator enhancements, and a human-following bot to enhance independence, save costs, secure purchases, and overall improve the quality of life for people with disabilities.",
+    "100",
+  ],
+  [
+    "How might we develop a suitable technology to track deep-sea fishermen or their locations, ensuring effective monitoring and enhancing safety measures in maritime activities?",
+    "101",
+  ],
+];
+const smartCity = [
+  [
+    "How might an innovative solution address communication challenges in remote areas, such as hilly and forest regions with weak or no network coverage? The goal is to develop an affordable and accessible emergency communication system, considering that satellite phones are financially unfeasible for most of the population.",
+    "116",
+  ],
+  [
+    "How might we create an innovative drone system utilizing AI for automatic human detection in disaster situations, generating alarms, and efficiently dropping payloads, integrating technologies like cameras, processors, and payload mechanisms?",
+    "117",
+  ],
+  [
+    "How might we develop a reliable explosion risk detection system for the oil and gas industries that can effectively identify potential threat zones and issue early warnings to nearby workers?",
+    "118",
+  ],
+  [
+    "ow might we develop impactful solutions using Government Land Information System (GLIS) data to address urban planning, infrastructure, environmental conservation, land governance, and socio-economic challenges? Enabling evidence-based decision-making for sustainable development.",
+    "119",
+  ],
+  [
+    "How might we design a tool to assess an organization's ransomware readiness? Emphasizing prevention, detection, usability, and reporting to enhance overall resilience against potential cyber threats.",
+    "120",
+  ],
+  [
+    "How might we develop a blockchain-based application to identify and counter fake social media profiles? Tailored for investigative agencies and law enforcement, enhancing profile authenticity and security.",
+    "121",
+  ],
+  [
+    "How might we develop AI/ML-based technology to efficiently detect avalanche victims in harsh conditions? Exploring innovative ground-penetrating radar alternatives for improved accuracy and response.",
+    "122",
+  ],
+  [
+    "How might we develop an Autonomous Firefighting Drone to overcome challenges in firefighting, addressing urban traffic, narrow streets, and remote terrains? The goal is to enhance efficiency, reduce response times, and overcome access limitations.",
+    "123",
+  ],
+  [
+    "How to solve the challenge of honeycombing in high-rise buildings with a 100-meter elevation by developing innovative construction techniques or materials that enhance structural integrity and ensure the safety and durability of tall structures.",
+    "102",
+  ],
+  [
+    "How might we develop an AI-ML-based GIS application, utilizing open-source software, to analyze past flood imageries and project new images for specific flood levels? Aiding spatial assessment in flood-prone areas for enhanced rescue and relief efforts.\r\n\r\n\r\n\r\n\r\n",
+    "103",
+  ],
+  [
+    "How might we develop an automatic self-cleaning toilet system, incorporating UV disinfection, hot air drying, water-saving features, and real-time availability tracking for nearby functional toilets?\r\n\r\n\r\n\r\n\r\n",
+    "104",
+  ],
+  [
+    "How might we create prosthetic solutions for lower limb amputees, enabling motorcycle riding? Emphasizing brake and gear operation aligned with ankle movements for a seamless and adaptive experience.",
+    "105",
+  ],
+  [
+    "How might we create an AI-powered sentiment analysis solution specialized in interpreting emotions within social media content? Empowering individuals and organizations to manage online reputation and perception effectively.\r\n\r\n\r\n\r\n\r\n",
+    "106",
+  ],
+  [
+    "How might we develop tech solutions to support undertrial prisoners in India? Designing a mobile app for legal aid, connecting them with lawyers, clinics, and providing rehabilitation through education, vocational training, and mental health support.\r\n\r\n\r\n\r\n\r\n",
+    "107",
+  ],
+  [
+    "How might we develop an automated system for real-time street light fault detection, precise location tracking, and efficient maintenance in cities to enhance urban lighting infrastructure?\r\n\r\n\r\n\r\n\r\n",
+    "108",
+  ],
+  [
+    "How might we develop a user-friendly digital assistant providing legal information in multiple languages? Aiming to enhance accessibility and improve legal awareness among marginalized communities in India.\r\n\r\n\r\n\r\n\r\n",
+    "109",
+  ],
+  [
+    "How we innovate in designing an, cost-effective shelter solution to shield Live Stocks from harsh weather conditions during rainy seasons, mitigating health risks, minimizing hoof problems, and fostering herd well-being and productivity.",
+    "110",
+  ],
+  [
+    "How might we design a GIS-based solution to identify and manage harmful plants infestation, ensuring real-time updates on infestation patterns, and addressing ecological and socio-economic impacts?",
+    "111",
+  ],
+  [
+    "How can we create a chatbot that can provide reliable and relevant information about various government schemes to the citizens, and help them identify their eligibility criteria for different programs, using an intuitive interface, engaging conversation, and diverse scenarios?",
+    "112",
+  ],
+  [
+    "How might we develop a unique platform to market traditional products, supporting the livelihood of rural artisans and pottery makers, fostering economic sustainability and preserving cultural heritage?",
+    "113",
+  ],
+  [
+    "How might we create an AI-powered nutrition analyzer for fitness enthusiasts, revolutionizing dietary tracking and personalizing nutritional insights to optimize health and fitness goals effectively?",
+    "114",
+  ],
+  [
+    "How might the utilization of the digital twin technology for virtual city creation seeks to enhance urban planning, decision-making, and resource management, providing a cost-effective solution for more efficient and informed urban development.",
+    "115",
+  ],
+];
+const wasteManagement = [
+  [
+    "How might we develop a sustainable and eco-friendly method to create biodegradable/edible food packaging materials from fish waste using marine biopolymers to replace synthetic plastics, mitigating pollution in land, water, and air?",
+    "124",
+  ],
+  [
+    "How might we innovate solutions for the proper disposal of sanitary waste, ensuring environmental safety, cost-effectiveness, and scalability across diverse settings such as villages and educational institutions?",
+    "125",
+  ],
+  [
+    "How might we create an integrated e-waste management chain within educational institutions through collaborative industry partnerships? The project aims to collect, segregate, process, recycle, and reuse electronic waste, promoting environmental sustainability while generating financial returns.",
+    "126",
+  ],
+  [
+    "How can we create a process to transform non-biodegradable products into useful ones, mitigating environmental impact and promoting sustainable solutions?",
+    "127",
+  ],
+];
+const waterSoilConservation = [
+  [
+    "How might we address the significant problem of contamination in groundwater by seeking solutions for detection, remediation, and prevention and the goal is to safeguard public health and ensure the sustainability of groundwater resources in affected areas.",
+    "128",
+  ],
+  [
+    "How might we develop a solution to combat global water depletion by creating an efficient water distribution network? The solution involves implementing a cloud-based dashboard for analytics, improving service, enhancing repair efficiency, reducing leakage, enabling GIS mapping, and overcoming deployment constraints for accurate water supply line assessment.",
+    "129",
+  ],
+  [
+    "How might we address the excessive water usage in textile fabric dyeing, seeking solutions to significantly reduce water consumption during the dyeing process?",
+    "130",
+  ],
+];
+export const titlesMap = {
+  "Agritech & Food Technology": agri,
+  "Clean and Green Technology": cleanAndGreen,
+  "Education 4.0": educationFourPointO,
+  "Health/Med Tech": healthMedTech,
+  "Heritage & Culture": heritage,
+  "Renewable & Sustainable Energy": renewableAndSustainableEnergy,
+  "Smart Manufacturing": smartManufacturing,
+  "Smart Town/City": smartCity,
+  "Waste Management": wasteManagement,
+  "Water & Soil Conservation": waterSoilConservation,
+};
